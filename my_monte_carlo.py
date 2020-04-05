96e1f0eb3755ed1d59421a2f0d3415bef78e0842
Alon Rafael Amedi <alonamedi@gmail.com>
2020-04-05 20:42:55 +0300

    value iteration and monte carlo

---
+++ my_monte_carlo.py
@@ -0,0 +1,87 @@
+from collections import defaultdict
+import numpy as np
+from grid_world import standard_grid, negative_grid
+from iterative_policy_evaluation import print_values, print_policy
+
+# constants
+TH = 10e-3
+GAMMA = 0.9
+POSSIBLE_ACTIONS = ('U', 'D', 'L', 'R')
+
+
+def play_game(grid, policy, gamma):
+    """
+    Simulates a game run to evaluate a given policy on a grid.
+    we are using the "exploring starts method" to randomly start the game from different states.
+    This is required to be able to visit states we are not suppose to visit when using deterministic policy as we do
+    which is - go through the upper left path or towards the losing state)
+
+    :param grid: a Grid instance
+    :param policy: a dict where keys are states and values are actions
+    :param gamma: the discount factor to calculate the returns
+    :return: a list of (state, return) pairs
+    """
+
+    # exploring start
+    actionable_states = list(grid.actions.keys())
+    start_idx = np.random.choice(len(actionable_states))
+    start_state = actionable_states[start_idx]
+    grid.set_state(start_state)
+
+    states_and_rewards = [(grid.current_state(), 0)]  # list of tuples of (state, reward)
+    while not grid.game_over():
+        a = policy[grid.current_state()]
+        r = grid.move(a)
+        states_and_rewards.append((grid.current_state(), r))
+
+    g = 0
+    states_and_returns = []
+    for i, (s, r) in enumerate(reversed(states_and_rewards)):
+        if i > 0:
+            states_and_returns.append((s, g))
+        g = r + (gamma * g)
+
+    states_and_returns.reverse()
+    return states_and_returns
+
+
+if __name__ == "__main__":
+    grid = standard_grid()
+
+    ### fixed policy ###
+    policy = {
+        (2, 0): 'U',
+        (1, 0): 'U',
+        (0, 0): 'R',
+        (0, 1): 'R',
+        (0, 2): 'R',
+        (1, 2): 'R',
+        (2, 1): 'R',
+        (2, 2): 'R',
+        (2, 3): 'U',
+    }
+
+    states = grid.all_states()
+    V = defaultdict(float)
+    all_returns = defaultdict(list)
+    N = 100
+    for _ in range(N):
+        already_seen = defaultdict(bool)
+        states_and_returns = play_game(grid, policy, GAMMA)
+        for s, g in states_and_returns:
+            # calculate the reward from states_and_rewards by 1st visit method
+            if s not in already_seen:
+                all_returns[s].append(g)
+                V[s] = float(np.mean(all_returns[s]))
+                print(V[s])
+                already_seen[s] = True
+
+    print("Rewards:")
+    print_values(grid.rewards, grid)
+    print("Values:")
+    print_values(V, grid)
+    print("Policy:")
+    print_policy(policy, grid)
+
+
+
---
+++ my_value_iteration.py
@@ -0,0 +1,115 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from grid_world import standard_grid, negative_grid
+from iterative_policy_evaluation import print_policy, print_values
+from collections import defaultdict
+
+"""
+Note the changes - 
+1. 
+2. We are no longer decide according to policy convergence but by state-value convergence
+3. the convergence flag moved to the iterative_policy_eval finction which now needs to know about the possible actions 
+    in order to find max value over actions rather argmax value
+"""
+# constants
+TH = 10e-4
+GAMMA = 0.9
+POSSIBLE_ACTIONS = ('U', 'D', 'L', 'R')
+
+
+def iterative_policy_eval(g, gamma, state_values, policy, th, allowed_actions):
+    g_states = g.all_states()
+    values = state_values.copy()
+
+    biggest_change = 0
+    is_converged = False
+    for s in g_states:
+        old_v = values[s]
+        new_value = float('-inf')
+        # V(s) only has value if it's not a terminal state
+        if s in policy:
+            for a in allowed_actions:
+                g.set_state(s)
+                r = g.move(a)
+                v = r + gamma * values[g.current_state()]
+                if v > new_value:
+                    new_value = v
+            values[s] = new_value
+        biggest_change = max(biggest_change, np.abs(old_v - values[s]))
+
+    if biggest_change < th:
+        is_converged = True
+    print("values for given policy:")
+    print_values(values, g)
+    print("\n\n")
+    return is_converged, values
+
+
+def imporve_policy(grid, policy, steps_values, gamma, allowed_actions):
+
+    improved_policy = policy.copy()
+    is_converged = True
+    for s, current_action in improved_policy.items():
+        best_val = float('-inf')
+        new_action = current_action  # just to have a place holder for best action found
+
+        # iterate over possible actions to find the action that provide the best state value
+        for candidate_action in allowed_actions:
+            grid.set_state(s)
+            r = grid.move(candidate_action)
+            v = r + gamma * steps_values[grid.current_state()]
+            if v > best_val:
+                best_val = v
+                new_action = candidate_action
+        improved_policy[s] = new_action
+
+    return improved_policy
+
+
+if __name__ == "__main__":
+
+    # grid = standard_grid()
+    grid = negative_grid()
+    print_values(grid.rewards, grid)
+
+    # Step 1: initialize policy and state values
+    policy = dict(zip(grid.actions.keys(), np.random.choice(POSSIBLE_ACTIONS, len(grid.actions.keys()))))
+
+    V = defaultdict(int)
+    for s in grid.actions:
+        V[s] = np.random.random()
+
+    print("Initialized values:")
+    print_values(V, grid)
+    print("Initialized pplicy:")
+    print_policy(policy, grid)
+
+    while True:
+        # Step 2: evaluate V(s) for current policy
+        print(f"Evaluating V(s) for current policy:")
+        print_policy(policy, grid)
+        is_policy_converged, V = iterative_policy_eval(grid, GAMMA, V, policy, TH, POSSIBLE_ACTIONS)
+        print("Evaluated V(s):")
+        print_values(V, grid)
+
+        if is_policy_converged:
+            break
+
+        # Step 3: improve policy - go over each step in the policy
+        policy = imporve_policy(grid, policy, V, GAMMA, POSSIBLE_ACTIONS)
+
+    print("Values:")
+    print_values(V, grid)
+    print("Policy:")
+    print_policy(policy, grid)
+
+
+
+
+
+
+
+
+
+
+
